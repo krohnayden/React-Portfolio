5d94737198c3ee432f760aebda4fc57b8ae896f1
Ayden Krohn <krohnayden@gmail.com>
2024-02-21 15:55:53 -0600

    added portfolio component and styling

--- React-Portfolio/src/components/body/resume.jsx
+++ React-Portfolio/src/components/body/resume.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-//Add resume file
-// import resumePDF from '../../assets/'
+import resumePDF from '../../assets/Resume.pdf'
+import resumeLogo from '../../assets/resumeLogo.png'
+import Skills from '../body/skills'
+
 
 export default function Resume() {
     return (
         <div>
-            
+            <section className='welcomeSection'>
+                <div>
+                    <h2 className='resumeIntro'>Download My Resume</h2>
+                </div>
+                <div className='resumeDiv'>
+                    <a href={resumePDF} target='_blank' rel="noreferrer"><img src={resumeLogo} className='resumeLogo'/></a>
+
+                    <p className='resumeCaption'>Click to View Resume</p>
+                </div>
+                <div className='skillsContainer'>
+                <p>Explore my Related Skills Below!</p>
+                    <Skills />
+                </div>
+            </section>
         </div>
     )
 }
--- React-Portfolio/src/components/body/skills.jsx
+++ React-Portfolio/src/components/body/skills.jsx
@@ -2,8 +2,48 @@ import React from "react";
 
 export default function Skills() {
     return (
-        <div>
-            
+        <div className='skillsSection'>
+            <div className='frontEndDiv'>
+                <h3 className='frontEndTitle'>Front-End Tech</h3>
+                <p className='frontEndColumn'>
+                    <ul className='skillsList'>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>Bootstrap</li>
+                        <li>API's</li>
+                        <li>GIT</li>
+                    </ul>
+                </p>
+            </div>
+            <div className='backEndDiv'>
+                <h3 className='backEndTitle'>Back-End Tech</h3>
+                <p className='backEndColumn'>
+                    <ul className='skillsList'>
+                        <li>Node.js</li>
+                        <li>Jest</li>
+                        <li>Express.js</li>
+                        <li>MySQL</li>
+                        <li>Object-Relational Mapping(ORM)</li>
+                        <li>Sequelize</li>
+                        <li>Model-View-Controller (MVC)</li>
+                    </ul>
+                </p>
+            </div>
+            <div className='databaseTechDiv'>
+                <h3 className='databaseTechTitle'>Database Tech</h3>
+                <p className='databaseTechColumn'>
+                    <ul className='skillsList'>
+                        <li>Mongoose</li>
+                        <li>GraphQL</li>
+                        <li>Progessive Web Applications (PWA)</li>
+                        <li>MongoDB</li>
+                        <li>MERN</li>
+                        <li>State</li>
+                        <li>React</li>
+                    </ul>
+                </p>
+            </div>
         </div>
     )
 }
